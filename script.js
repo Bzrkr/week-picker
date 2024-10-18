f89493eb3820ade203a889f9809e6fdf865eaefa
Alex <42751891+Bzrkr@users.noreply.github.com>
2024-10-18 11:11:51 +0300

    Update script.js

--- script.js
+++ script.js
@@ -20,7 +20,7 @@
             const year = date.getFullYear();
             const month = String(date.getMonth() + 1).padStart(2, '0');
             const day = String(date.getDate()).padStart(2, '0');
-            return `${day}-${month}-${year}`;
+            return `${year}-${month}-${day}`;
         }
 
         function getWeekNumber(currentWeek, diffDays) {
